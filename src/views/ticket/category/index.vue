a5bbae36b5136181c97f144aa438b39a4159cb60
kyesheu <jaykai5991@gmail.com>
2026-06-29 17:58:06 +0800

    feat: initial frontend setup with ticket module views and gitignore

---
+++ src/views/ticket/category/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    <el-card>
+      <template #header>
+        <span>分类管理</span>
+      </template>
+      <div>工单分类管理页面 - 待开发</div>
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts" name="Category">
+// TODO: 实现分类管理页面
+</script>
---
+++ src/views/ticket/ticket/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    <el-card>
+      <template #header>
+        <span>工单列表</span>
+      </template>
+      <div>工单列表页面 - 待开发</div>
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts" name="Ticket">
+// TODO: 实现工单列表页面
+</script>
